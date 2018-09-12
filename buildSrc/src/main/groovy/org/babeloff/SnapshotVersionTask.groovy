fbc259b9a1cfde6169f631be6f0d90e1019f5983
Fred Eisele <fredrick.eisele@gmail.com>
2018-09-12 15:20:18 -0500

    preparing for gradle modules

--- buildSrc/src/main/groovy/org/babeloff/SnapshotVersionTask.groovy
+++ buildSrc/src/main/groovy/org/babeloff/SnapshotVersionTask.groovy
@@ -1,24 +1,28 @@
 package org.babeloff
 
+import java.time.LocalDate
+
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 
-class ReleaseVersionTask extends DefaultTask {
-    @Input Boolean release
-    @OutputFile File destFile
+class SnapshotVersionTask extends DefaultTask {
+    @OutputFile File propFile
 
-    ReleaseVersionTask() {
+    SnapshotVersionTask() {
         group = 'versioning'
         description = 'Makes project a release version.'
     }
 
     @TaskAction
     void start() {
-        project.version.release = true
-        ant.propertyfile(file: destFile) {
-            entry(key: 'release', type: 'string', operation: '=', value: 'true')
+        def date = LocalDate.now()
+        ant.propertyfile(file: propFile) {
+            entry(key: 'year', type: 'string', operation: '=', value: date.getYear().toString())
+            entry(key: 'month', type: 'string', operation: '=', value: date.getMonthValue().toString())
+            entry(key: 'day', type: 'string', operation: '=', value: date.getDayOfMonth())
+            entry(key: 'release', type: 'string', operation: '=', value: 'false')
         }
     }
 }
